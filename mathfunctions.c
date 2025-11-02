796da7e01ff483dcb28d41f42ce3836e3dea4397
Marina <m.ivanova@emai.cz>
2025-11-02 20:37:49 +0100

    mathfunctionstests

--- mathfunctions.c
+++ mathfunctions.c
@@ -6,7 +6,19 @@ int main() {
     int x = 9;
 
     x = sqrt(x);
+    // x = pow(x, 2);
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = abs(x);
+    // x = log(x);
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x); 
 
-    printf("%d", x);
+
+    printf("%d\n", x);
+
+    return 0;
 
 }
